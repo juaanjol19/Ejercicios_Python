e1b4a2881378d883dccc6e5de25165a469d02c58
juaanjol19 <105843432+juaanjol19@users.noreply.github.com>
2023-02-05 18:33:30 -0600

    Add files via upload

---
+++ Ej2_print.py
@@ -0,0 +1,21 @@
+print("hola este es mi primer print")
+
+print("hola este es mi primer print")
+
+print("hola este es mi primer print",end='\n')
+
+print("pythn","es","la","ostia",end='\n')
+print("pythn","es","la","ostia",end=' ',sep="**")
+print()
+print("Hola {} como estas".format("juan"))
+print("tu nombres es {} y tienes {} años".format("juna",19))
+print(f'tu nombre es {"juan"} y tiens {19} años')
+
+
+
+numeros = [1,2,3,4,5,6,7,8,9]
+
+print(numeros)
+
+dict = {"Equipo":"America","Ciudad":"Mexico"}
+print(dict)
