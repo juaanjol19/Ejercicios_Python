da327f63fb4aa258635f008a607973cbefec2772
juaanjol19 <105843432+juaanjol19@users.noreply.github.com>
2023-02-08 23:36:34 -0600

    Add files via upload

---
+++ Ej8_PrimerYultimoElemento.py
@@ -0,0 +1,9 @@
+# Obtener el primero y el ultimo elemento de una lista
+
+lista = ['Messi','Cristinao','Mbappe','Haland','Neymar']
+primer_elemento = lista[0]
+ultimo_elemento = lista[-1]
+ultimo_elemento2 = lista[len(lista)-1]
+
+print(primer_elemento,ultimo_elemento)
+print(primer_elemento,ultimo_elemento2)
